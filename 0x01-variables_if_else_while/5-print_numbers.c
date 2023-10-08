ab5c56b38013f108c49248b8b544f902c32880fa
ahmed <ray2legand128@gmail.com>
2023-10-08 16:38:13 -0700

    17

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ * This code prints the numbers from 0 to 9 consecutively.
+ * Code made by Ahmed Magdy
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
