a6c0872f1d6c6537eb6f3ff266467a7d7c91a53f
ahmed <ray2legand128@gmail.com>
2023-10-10 16:06:59 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * This function prints the last digit of a number 'a'.
+ *
+ * @a: The number to extract the last digit from
+ *
+ * Return: The last digit of 'a'.
+ * Code made by Ahmed Magdy.
+ * ALX CO 1 blended
+ */
+int print_last_digit(int a)
+{
+	int b = a % 10;
+
+	return (b);
+
+}
+
