ac2e2e891f9f86ae094170fcaa8418090c534d2d
ahmed <ray2legand128@gmail.com>
2023-10-10 14:18:55 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
-*void print_alphabet_x10 - this code to print
-*the alphbet 10 times
-*
-*This code is made to print the alphbet  10 time.
-*Code made by Ahmed Magdy.
-*ALX CO 1 blended
-*/
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * This code is made to print the alphabet 10 times.
+ *
+ * Code made by Ahmed Magdy.
+ * ALX CO 1 blended
+ */
 void print_alphabet_x10(void)
 {
-int n = 1;
-	while (n <= 10)
-{
-         char c = 'a';
+	int n = 1;
+	char c;
 
-	while (c <= 'z')
-{
-	_putchar(c);
-	c++;
-}	
-}
- 	-putchar('\n')
+	while (n <= 10)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		n++;
+	}
+	_putchar('\n');
 }
 
