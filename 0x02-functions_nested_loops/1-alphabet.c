ac40cfb9d64782e171e43dc58dbf1a33efe91729
ahmed <ray2legand128@gmail.com>
2023-10-10 13:02:56 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,25 @@
-1
+# include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ * This code is made to print the alphbet  one time.
+ * Code made by Ahmed Magdy.
+ */
+
+int main(void)
+{
+
+	char c ='a';
+
+	while (c <= 'z')
+{
+	_putchar(c)
+	c++;
+}
+
+
+	return (0);
+}
+
