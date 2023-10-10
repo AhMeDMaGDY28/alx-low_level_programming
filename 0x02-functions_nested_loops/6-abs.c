c94b242d1c25acf4e3ec910173f8ef7ce3cb73c3
ahmed <ray2legand128@gmail.com>
2023-10-10 15:36:35 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * This function computes the absolute value of an integer 'a'.
+ *
+ * @a: The integer to compute the absolute value for
+ *
+ * Return: The absolute value of 'a'.
+ * Code made by Ahmed Magdy.
+ * ALX CO 1 blended
+ */
+int _abs(int a)
+{
+	int b = -1;
+	int c = a * b;
+
+	if (a < 0)
+	{
+		_putchar('c');
+	}
+	else
+	{
+		_putchar('a');
+	}
+	return (c);
+}
+
