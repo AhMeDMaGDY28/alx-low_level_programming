9e9c2568f04dcfe1d463e85d4cb37b55016aae43
ahmed <ray2legand128@gmail.com>
2023-10-10 08:13:35 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * this code is made to  print
+ * _putchar one time
+ * Return: Always 0
+ * code made by ahmed magdy
+*/
+
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the day of the nested loops and functions i hope it is easy
