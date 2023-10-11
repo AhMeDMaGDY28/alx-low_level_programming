baad99f14e6621e38dff9bd1fcabb1a50f09cba9
ahmed <ray2legand128@gmail.com>
2023-10-10 19:30:43 -0700

    16.7

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,8 +3,9 @@
 /**
  * main - Entry point of the program.
  *
- * Description: Prints the first 98 Fibonacci numbers separated by comma
- * and space, starting with 1 and 2, followed by a new line.
+ * Description: Prints the first 98 Fibonacci numbers whose values do not
+ * exceed 4,000,000, starting with 1 and 2, separated by a comma and space,
+ * followed by a new line.
  *
  * Code made by Ahmed Magdy.
  * ALX CO 1 blended.
@@ -13,20 +14,27 @@
  */
 int main(void)
 {
-	unsigned long a = 1, b = 2, nextTerm;
-	int i;
+	unsigned long a = 1, b = 2, nextTerm, sum = 2;
 
 	printf("%lu, %lu", a, b);
 
-	for (i = 3; i <= 98; i++)
+	while (1)
 	{
 		nextTerm = a + b;
+
+		if (nextTerm > 4000000)
+			break;
+
+		if (nextTerm % 2 == 0)
+			sum += nextTerm;
+
 		printf(", %lu", nextTerm);
+
 		a = b;
 		b = nextTerm;
 	}
 
-	printf("\n");
+	printf("\nSum of even Fibonacci numbers: %lu\n", sum);
 
 	return (0);
 }
