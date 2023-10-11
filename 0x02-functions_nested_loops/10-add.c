4d90406e4942c51da6693dc0b330ab10b6c147f4
ahmed <ray2legand128@gmail.com>
2023-10-10 17:09:53 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Description: This function adds two integers and returns the result.
+ *
+ * Return: The sum of a and b.
+ * Code made by Ahmed Magdy.
+ * ALX CO 1 blended
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	_putchar('0' + sum);
+	return (sum);
+}
+
