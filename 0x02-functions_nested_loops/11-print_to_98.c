463519467eafc73d87cf6ac08d93cb75ac134e18
ahmed <ray2legand128@gmail.com>
2023-10-10 18:15:00 -0700

    10.8

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,34 +9,29 @@
  * Code made by Ahmed Magdy.
  * ALX CO 1 blended
  */
+
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
-			_putchar(n + '0');
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", n);
 			n++;
 		}
+		printf("98\n");
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("98\n");
 	}
 	else
 	{
-		while (n >= 98)
-		{
-			_putchar(n + '0');
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			n--;
-		}
+		printf("98\n");
 	}
-	_putchar('\n');
 }
-
