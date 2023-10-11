e778f2615da507a96d5ed8d5df1a7306b16a41d7
ahmed <ray2legand128@gmail.com>
2023-10-10 18:08:14 -0700

    11.5

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,37 +15,25 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			if (i < 98)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(i + '0');
-				_putchar('\n');
-			}
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			if (i > 98)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(i + '0');
-				_putchar('\n');
-			}
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
+
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
 
