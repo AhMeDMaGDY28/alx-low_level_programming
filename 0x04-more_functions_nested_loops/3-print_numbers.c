b63699a83bbe99ad1549f922c351a05d9814eb69
ahmed <ray2legand128@gmail.com>
2023-10-12 08:10:34 -0700

    12

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -13,13 +13,11 @@ void print_numbers(void);
 {
 	int i;
 
-	i = 1
-
 	char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-	for (i > 0; i < 11; i++)
+	for (i = 1; i < 11; i++)
 	{
-		_putchar(c[]);
+		_putchar(c[i]);
 	}
 	_putchar('\n');
 	return (0);
