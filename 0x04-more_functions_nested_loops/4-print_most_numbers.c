87821fdb859f8f4f489bb4b1eae16a0f8f99903a
ahmed <ray2legand128@gmail.com>
2023-10-12 09:01:42 -0700

    16

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers from 0 to 9 (excluding 2 and 4).
+ *
+ * Description: This function prints the numbers from 0 to 9, excluding 2 and 4,
+ * to the standard output using the _putchar function.
+ * ALX CO1 BLENDED
+ * code made by ahmed magdy
+ */
+
+void print_most_numbers(void)
+{
+	int i ;
+	for (i = 0; i <= 10 ;i++)
+	{
+		while (i != 2 || i != 4)
+		{
+			_putchar(i + '0';)
+		}
+	}
+	_putchar('\n');
+}
+
