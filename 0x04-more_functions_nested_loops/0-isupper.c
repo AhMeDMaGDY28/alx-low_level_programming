b7ddc08af0554b8e8ab2b61c5ca133627c2fe50c
ahmed <ray2legand128@gmail.com>
2023-10-12 06:41:05 -0700

    6

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - check if uppercase
- *this code check for if a is uppercase
- * return 1 or 0
+ * _isupper - check if a character is uppercase
+ * this code check for if c is uppercase
+ * @c: The character to be checked
  * code made by ahmed magdy
  * ALX CO 1 BLENDED
+ * Return: 1 if the character is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
