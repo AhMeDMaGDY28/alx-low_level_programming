05cb9da633a4c67d531fd89dec22fe5303c137cb
ahmed <ray2legand128@gmail.com>
2023-10-12 07:30:45 -0700

    9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two integers.
+ *
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of multiplying 'a' and 'b'.
+ * made by ahmed magdy
+ * AlX CO1 BLENDED
+ */
+
+int mul(int a, int b);
+{
+	int n = a * b;
+
+	printf("%d", n);
+	return (0);
+}
+
