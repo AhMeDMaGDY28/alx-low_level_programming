22a2bdcd782dc2d18d2df7db8463d1ce4d59c9a5
Ahmed <ray2legand128@gmail.com>
2023-10-12 22:09:34 +0300

    21

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,31 @@
 #include "main.h"
 
 /*
- *more_numbers - Print the numbers from 0 to 14
- *ALX CO1 BLENDED
- *code made by ahmed magdy
+ * more_numbers - Print the numbers from 0 to 14
+ * Description: This function prints numbers from 0 to 14, ten times, using
+ * the _putchar function. Each sequence is followed by a newline character.
+ * ALX CO1 BLENDED
+ * code made by ahmed magdy
+ * Return: No return value (void).
  */
 
 void more_numbers(void)
 {
 	int i;
+	int a = 1;
 
-	for (i = 0; i < 15; i++)
+	while (a <= 10)
 	{
-		_putchar(i + '0');
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar('1');
+			}
+
+			_putchar(i + '0');
+		}
+		a++;
 	}
 	_putchar('\n');
 }
