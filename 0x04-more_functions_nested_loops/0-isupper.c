a7137e7cc947e527795dfaa641cc1fc9bd8c11ac
ahmed <ray2legand128@gmail.com>
2023-10-12 04:42:06 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - check if uppercase
+ *this code check for if a is uppercase
+ * return 1 or 0
+ * code made by ahmed magdy
+ * ALX CO 1 BLENDED
+ */
+
+int _isupper(int c)
+{
+
+	int a = c;
+
+	if(a >=65 && a <=90)
+	{
+		return (1); 
+	}
+	else
+	{
+		return(0);
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the 0x04. C - More functions, more nested loops project
