a6630ea177d4cb9c13d02b9a226f8c929707f61a
ahmed <ray2legand128@gmail.com>
2023-10-12 08:01:38 -0700

    10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 to 9.
+ *
+ * Description: This function prints the numbers from 0 to 9 to the standard
+ * output using the _putchar function.
+ * code made by ahmed magdy
+ * ALX CO1 BLENDED
+ */
+
+void print_numbers(void);
+{
+	int i = 1;
+
+	char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	for (i > 0; i < 11; i++)
+	{
+		_putchar(c[]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
