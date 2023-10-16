6692c513b3b0f564ef1866e597342e1652b55cf8
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-17 01:53:23 +0300

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse to the standard output.
+ *
+ * @s: A pointer to the string to be printed in reverse.
+ *
+ * Description: This function calculates
+ * the length of the string pointed to by @s,
+ * and then prints the characters of the string
+ * in reverse order to the standard output,
+ * followed by a newline character.
+ *
+ * Return: No return value (void).
+ *
+ * Author: Ahmed Magdy
+ * School: ALX CO 1 BLENDED
+ */
+void print_rev(char *s)
+{
+	int length = 0, i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	 i = length - 1;
+
+	while (i >= 0)
+	{
+		putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}
