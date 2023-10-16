01035a1ebd1421bf021796ec83ad68e49efc87e5
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-17 00:43:58 +0300

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - it changes the pointer and give it the value of 98 .
+ *
+ * @n: this for the pointer which will take the value of 98.
+ *
+ * Return: 1 if the character is a digit (0-9), 0 otherwise.
+ * made by ahmed magdy
+ * ALX CO 1 BLENDED
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
