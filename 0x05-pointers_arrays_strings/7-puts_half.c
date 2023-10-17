3ec1ac2ca53beed161951836d9416bdb0d20c168
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-17 03:24:22 +0300

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * puts_half - Prints the second half of a string to the standard output.
+ *
+ * @str: A pointer to the string to be processed.
+ *
+ * Description: This function calculates the
+ * length of the string pointed to by @str,
+ * and then prints the second half of the string to the standard output,
+ * followed by a newline character.
+ *
+ * Return: No return value (void).
+ *
+ * Author: Ahmed Magdy
+ * School: ALX CO 1 BLENDED
+ */
+void puts_half(char *str)
+{
+	int length = 0, i, b, a = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	i = length - 1;
+	b = i;
+	if (i % 2 == 0)
+	{
+		a = i / 2;
+	}
+	else
+	{
+		a = (i - 1) / 2;
+	}
+	while (a <= b)
+	{
+		a++;
+		putchar(str[a]);
+	}
+}
