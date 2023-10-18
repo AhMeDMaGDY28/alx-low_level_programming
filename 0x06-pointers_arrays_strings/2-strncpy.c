9bcf0ddeb833a556999cdaf732e4b9b3fa68bc2a
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-18 23:55:50 +0300

    getting files ready to begin

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - it changes the pointer and give it the value of 98 .
+ *
+ * @n: this for the pointer which will take the value of 98.
+ *
+ * Return: 1 if the character is a digit (0-9), 0 otherwise.
+ * made by ahmed magdy
+ * ALX CO 1 BLENDED
+*/
+char *_strncpy(char *dest, char *src, int n);
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - it changes the pointer and give it the value of 98 .
+ *
+ * @n: this for the pointer which will take the value of 98.
+ *
+ * Return: 1 if the character is a digit (0-9), 0 otherwise.
+ * made by ahmed magdy
+ * ALX CO 1 BLENDED
+*/
+void reverse_array(int *a, int n);
