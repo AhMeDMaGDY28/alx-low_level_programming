5abdc008cd88ecf394d374cb06af94ec2b142942
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-19 05:59:38 +0300

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,33 @@
 #include "main.h"
 /**
- * reset_to_98 - it changes the pointer and give it the value of 98 .
+ * _strncpy - Copies 'n' characters from the 'src' string to 'dest.'
  *
- * @n: this for the pointer which will take the value of 98.
+ * @dest: A pointer to the destination string where the copy will be stored.
+ * @src: A pointer to the source string from which characters will be copied.
+ * @n: The maximum number of characters to copy from 'src.'
  *
- * Return: 1 if the character is a digit (0-9), 0 otherwise.
- * made by ahmed magdy
- * ALX CO 1 BLENDED
-*/
+ * Description: This function copies up to
+ * 'n' characters from the 'src' string to the
+ * 'dest' string. If 'src' contains fewer
+ * than 'n' characters, the remaining characters in
+ * 'dest' are filled with null ('\0') characters.
+ *
+ * Return: A pointer to the destination string 'dest' after the copy.
+ *
+ * Author: Ahmed Magdy
+ * School: ALX CO 1 BLENDED
+ */
 char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
