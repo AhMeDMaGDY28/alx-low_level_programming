0db1ebdf85fb3ebe06d02687b93e2a1a0101e886
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-19 07:24:57 +0300

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,12 +1,37 @@
 #include "main.h"
 /**
- * reset_to_98 - it changes the pointer and give it the value of 98 .
+ * rot13 - Applies the ROT13 substitution cipher to a string.
  *
- * @n: this for the pointer which will take the value of 98.
+ * @s: A pointer to the string to be encrypted using ROT13.
  *
- * Return: 1 if the character is a digit (0-9), 0 otherwise.
- * made by ahmed magdy
- * ALX CO 1 BLENDED
-*/
+ * Description: This function takes a string pointed to by @s and applies
+ * the ROT13 substitution cipher to it. ROT13 is a simple letter substitution
+ * cipher that replaces a letter with the 13th letter after it in the alphabet.
+ * It works for both uppercase and lowercase letters, leaving non-alphabetical
+ * characters unchanged.
+ *
+ * Return: A pointer to the modified string @s with ROT13 applied.
+ *
+ * Author: Ahmed Magdy
+ * School: ALX CO 1 BLENDED
+ */
 
-char *rot13(char *)
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
