ccefdd17b21d8b115e63595542a42a3bbfdb2018
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-19 07:18:11 +0300

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,34 @@
 #include "main.h"
 /**
- * reset_to_98 - it changes the pointer and give it the value of 98 .
+ * cap_string - Capitalizes all words in a string.
+ * @str: The input string.
  *
- * @n: this for the pointer which will take the value of 98.
- *
- * Return: 1 if the character is a digit (0-9), 0 otherwise.
+ * Return: A pointer to the modified string.
  * made by ahmed magdy
  * ALX CO 1 BLENDED
 */
 
-char *cap_string(char *)
+char *cap_string(char *str)
+{
+	int idx = 0, j;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	while (str[idx])
+	{
+		if (str[idx] >= 'a' && str[idx] <= 'z')
+		{
+			if (idx == 0)
+				str[idx] -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (separators[j] == str[idx - 1])
+						str[idx] -= 'a' - 'A';
+				}
+			}
+		}
+		idx++;
+	}
+	return (str);
+}
