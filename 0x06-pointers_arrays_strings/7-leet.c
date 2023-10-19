94aed7c62bee2e49b35f02c58901d401e6a3bc63
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-19 07:21:36 +0300

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,33 @@
 #include "main.h"
 /**
- * reset_to_98 - it changes the pointer and give it the value of 98 .
+ * leet - Converts a string to "leet" (1337) language.
  *
- * @n: this for the pointer which will take the value of 98.
+ * @str: A pointer to the string to be transformed.
  *
- * Return: 1 if the character is a digit (0-9), 0 otherwise.
- * made by ahmed magdy
- * ALX CO 1 BLENDED
-*/
+ * Description: This function takes a string pointed to by 'str' and replaces
+ * certain characters with their corresponding "leet" (1337) characters.
+ * The characters replaced are 'A' or 'a' with '4', 'E' or 'e' with '3',
+ * 'O' or 'o' with '0', 'T' or 't' with '7', and 'L' or 'l' with '1'.
+ *
+ * Return: A pointer to the transformed string 'str'.
+ *
+ * Author: Ahmed Magdy
+ * School: ALX CO 1 BLENDED
+ */
 
-char *leet(char *)
+char *leet(char *str)
+{
+	int i, j;
+	char leetChars[] = "aAeEoOtTlL";
+	char leetReplacements[] = "4433007711";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (leetChars[j] == str[i])
+				str[i] = leetReplacements[j];
+		}
+	}
+	return (str);
+}
