f2a41ff4460f96cb206a28642edd424580079b0c
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-20 03:58:24 +0300

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,37 @@
 #include "main.h"
 /**
- * reset_to_98 - it changes the pointer and give it the value of 98 .
+ * main - Entry point of the program
  *
- * @n: this for the pointer which will take the value of 98.
+ * Description: This program demonstrates
+ * a pointer manipulation by changing the
+ * value of an integer pointed to by a
+ * pointer @p. It sets the value of the integer
+ * to 98 using pointer arithmetic, without directly modifying the variable @n.
  *
- * Return: 1 if the character is a digit (0-9), 0 otherwise.
- * made by ahmed magdy
- * ALX CO 1 BLENDED
-*/
+ * Return: Always returns 0 to indicate successful execution.
+ *
+ * Author: Ahmed Magdy
+ * School: ALX CO 1 BLENDED
+ */
 
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
