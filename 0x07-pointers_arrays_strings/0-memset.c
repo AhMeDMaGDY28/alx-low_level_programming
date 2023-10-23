8fe1c8c7049c5e2559c1bf56d1fcd5423a371d27
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-23 20:49:18 +0300

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,16 @@
+#include "main.h"
+
 char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = &b;
+
+		    unsigned x = (n - 1),
+	     i;
+
+	for (i = 0; i < x; i++)
+	{
+		s[i] = b;
+	}
+
+	return (p);
+}
