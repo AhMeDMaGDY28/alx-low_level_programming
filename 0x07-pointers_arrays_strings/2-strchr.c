025ff703acbad44584e7aaa6f7ad756b20a43ffa
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-23 22:57:14 +0300

    fix 4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -34,13 +34,13 @@ char *_strchr(char *s, char c)
 	}
 	else
 	{
-		for (i = 0; s[i] != '\0' i++)
+		for (i = 0; s[i] != '\0'; i++)
 		{
 			if (s[i] == c)
 			{
 				return (&s[i]);
 			}
 		}
-
-		return (NULL);
 	}
+	return (NULL);
+}
