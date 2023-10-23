51e12f9999750616b744048b67aa0c3c6b8763be
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-24 00:11:34 +0300

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,37 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring within a string.
+ *
+ * @haystack: A pointer to the null-terminated string to be searched.
+ * @needle: A pointer to the null-terminated substring to search for.
+ *
+ * Description: This function searches for the first
+ * occurrence of the null-terminated
+ * substring 'needle' in the null-terminated string
+ * 'haystack.' It iterates through the
+ * characters in 'haystack' and compares them to the
+ * characters in 'needle' to find a match.
+ * If a match is found, it returns a pointer to the
+ * location of the first character of the substring
+ * within the 'haystack.' If no match is found, it returns NULL.
+ *
+ * Return: A pointer to the first occurrence of the 'needle'
+ * substring in the 'haystack' string if found,
+ * or NULL if no match is found.
+ */
 char *_strstr(char *haystack, char *needle)
+{
+	int i, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (k = 0; needle[k] != '\0'; k++)
+		{
+			if (haystack[i] == needle[k] && haystack[i + 1] == needle[k + 1])
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return (NULL);
+}
