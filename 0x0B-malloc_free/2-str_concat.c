99585e5a3776902d7381ada8310ae404faee6b5c
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-31 00:18:07 +0200

    fix 4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,6 @@ char *str_concat(char *s1, char *s2)
 	int i, length_of_s2 = 0, length_of_s1 = 0;
 	char *p;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
