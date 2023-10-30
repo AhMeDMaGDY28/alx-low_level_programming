945f4c8305df764b92bc528eada8989c71517888
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-30 22:58:24 +0200

    test1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,7 +22,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	p = malloc(sizeof(int) * size);
+	p = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
