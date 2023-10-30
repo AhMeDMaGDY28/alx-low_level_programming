3a6d5b481c12360a40b0eec6be5fa853d01696a6
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-31 00:17:30 +0200

    fix 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,14 +18,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	if (s1 == NULL && s2 != NULL)
-	{
-		return (s2);
-	}
-	if (s1 != NULL && s2 == NULL)
-	{
-		return (s1);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		length_of_s1++;
