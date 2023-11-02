0f3d28f4ff671c52456c0224ac6a920b6233a290
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-02 21:09:35 +0200

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,6 +20,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int length_s1 = 0, length_s2 = 0;
 	char *p;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s1 == NULL)
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		length_s1++;
@@ -32,13 +40,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = length_s2;
 	}
-
 	p = (char *)malloc(sizeof(char) * (length_s1 + n + 1));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		p[i] = s1[i];
