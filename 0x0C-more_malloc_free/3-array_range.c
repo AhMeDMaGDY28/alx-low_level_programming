09aa9b112b840d3808ba5b2f1d05c9c8035ea377
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-03 02:25:18 +0200

    docemntion

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,5 +1,18 @@
 #include "main.h"
-
+/**
+ * array_range - Create an array of integers from min to max (inclusive).
+ * @min: The minimum value.
+ * @max: The maximum value.
+ *
+ * Description:
+ * This function creates an array of integers starting
+ * from the minimum value (min)
+ * up to the maximum value (max), inclusive. It allocates memory for the array.
+ *
+ * Return: A pointer to the created array, or NULL
+ * if min is greater than max or if
+ *         memory allocation fails.
+ */
 int *array_range(int min, int max)
 {
 	int i;
