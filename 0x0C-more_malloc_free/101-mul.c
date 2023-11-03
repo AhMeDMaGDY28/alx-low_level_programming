469abe796620bd7c2b8c23424597baa26399b706
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-03 02:15:59 +0200

    fix 1

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,88 +1,37 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _puts - prints a string followed by a new newline
- * @str: The string to print
- */
-void _puts(char *str)
-{
-	int index = 0;
-
-	while (str[index])
-	{
-		_putchar(str[index]);
-		index++;
-	}
-}
-
-/**
- * _atoi - converts a string to an int
- * @s: pointer to string
- * Return: The converted integer
- */
-int _atoi(const char *s)
-{
-	int sign = 1;
-	unsigned long int result = 0, first, index;
-
-	for (first = 0; !(s[first] >= 48 && s[first] <= 57); first++)
-	{
-		if (s[first] == '-')
-		{
-			sign *= -1;
-		}
-	}
-
-	for (index = first; s[index] >= 48 && s[index] <= 57; index++)
-	{
-		result *= 10;
-		result += (s[index] - 48);
-	}
-
-	return (sign * result);
-}
-
-/**
- * print_int - prints an integer
- * @n: The integer to print
- * Return: void
- */
-void print_int(unsigned long int n)
-{
-	unsigned long int divisor = 1;
-	unsigned long int index, result;
-
-	for (index = 0; n / divisor > 9; index++, divisor *= 10)
-	{
-	}
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		result = n / divisor;
-		_putchar('0' + result);
-	}
-}
-
-/**
- * main - returns the product of two positive numbers
+ * main - Multiply two numbers provided as command-line arguments.
  * @argc: Number of arguments
- * @argv: Arguments
- * Return: 0
+ * @argv: Array of arguments
+ * Return: 0 for success
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
+	unsigned long product;
+	int argIndex, charIndex;
 
-	if (argc != 3 || !_atoi(argv[1]) || !_atoi(argv[2]))
+	if (argc != 3)
 	{
-		_puts("Error\n");
+		printf("Error\n");
 		exit(98);
 	}
 
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
-	_putchar('\n');
+	for (argIndex = 1; argIndex < argc; argIndex++)
+	{
+		for (charIndex = 0; argv[argIndex][charIndex] != '\0'; charIndex++)
+		{
+			if (argv[argIndex][charIndex] > '9' || argv[argIndex][charIndex] < '0')
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
 
+	product = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", product);
 	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,4 +11,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 
 
+
 #endif
