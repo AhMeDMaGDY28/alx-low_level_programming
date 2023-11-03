ac9800fcb7e14635f324fa95e90e41d9829f719f
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-03 02:39:59 +0200

    fix 4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,24 +15,23 @@
  */
 int *array_range(int min, int max)
 {
-	{
-		int i, l;
-		int *s;
+	int i, l;
+	int *s;
 
-		if (min > max)
-		{
-			return (0);
-		}
-		i = max - min + 1;
-		s = malloc(sizeof(int) * i);
-		if (s == 0)
-		{
-			return (0);
-		}
-		for (l = 0; l < i; l++)
-		{
-			s[l] = min;
-			min++;
-		}
-		return (s);
+	if (min > max)
+	{
+		return (0);
 	}
+	i = max - min + 1;
+	s = malloc(sizeof(int) * i);
+	if (s == 0)
+	{
+		return (0);
+	}
+	for (l = 0; l < i; l++)
+	{
+		s[l] = min;
+		min++;
+	}
+	return (s);
+}
