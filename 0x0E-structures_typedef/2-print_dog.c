2f21a8b8641c3489d2063fa25e4e8d0c3f913e75
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-07 00:27:15 +0200

    2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,5 +2,14 @@
 
 void print_dog(struct dog *d)
 {
-	
+	if (d != NULL)
+	{
+		printf("name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		return;
+	}
 }
