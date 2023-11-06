4b1915e5662ce383ac971d06e93a4cfdf5cd4f5b
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-07 00:17:21 +0200

    fix 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,6 +5,13 @@
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
+ *
+ * Description:
+ * This function initializes a dog structure with the provided name, age, and owner.
+ * If the pointer to the dog structure (d) is not NULL, it sets the name, age, and owner
+ * for the dog. If d is NULL, the function does nothing.
+ *
+ * Return: No return value.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -13,5 +20,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->age = age;
 		d->name = name;
 		d->owner = owner;
+	}
+	else
+	{
+		return;
 	}
 }
