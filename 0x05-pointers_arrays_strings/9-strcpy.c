6ce02fc760b40fd999cee51711c8afd63af0eb20
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-07 02:19:19 +0200

    4-new_dog.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -18,6 +18,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+#include <string.h>
+/**
+ * new_dog - Create a new dog structure.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description:
+ * This function creates a new dog structure and initializes
+ *  its name, age, and owner
+ * using the provided parameters. It allocates memory
+ *  for the new structure and the
+ * name and owner strings.
+ *
+ * Return: A pointer to the new dog structure, or NULL if memory
+ *  allocation fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_1;
+	char *owner_1;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	name_1 = strdup(name);
+	owner_1 = strdup(owner);
+	if (name_1 == NULL || owner_1 == NULL)
+	{
+		free(new_dog);
+		free(name_1);
+		free(owner_1);
+		return (NULL);
+	}
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
