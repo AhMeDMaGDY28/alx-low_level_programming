5b6f88d8c8ab946c3880150614b48c590f2d6345
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-09 03:07:37 +0200

    fix 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -24,17 +24,22 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, p;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
+	if (array != NULL && cmp != NULL)
 	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			p = (*cmp)(array[i]);
+			if (p != 0)
+			{
+				return (i);
+			}
+		}
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
-	{
-		p = (*cmp)(array[i]);
-		if (p != 0)
-		{
-			return (i);
-		}
-	}
+
 	return (-1);
 }
