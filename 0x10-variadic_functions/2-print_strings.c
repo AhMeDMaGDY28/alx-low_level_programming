4e1a3a0087158b92069c4b1c0d76534ab38e1aba
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-09 22:43:37 +0200

    fix 1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		a = va_arg(args, char *);
-		if (args == NULL)
+		if (a == NULL)
 		{
 			printf("(nil)");
 		}
