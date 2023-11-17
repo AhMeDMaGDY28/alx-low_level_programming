25aa0e6c52796bccc2794148f5f933754a1bb6c9
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-17 03:39:47 +0200

    first advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * pre_main_function - Prints a two-line message to the standard output.
+ *
+ * Description: This function prints a two-line message to the standard output.
+ * It is intended to be executed before the main function.
+ * Author: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
+void pre_main_function(void)
+{
+	printf("%s\n%s\n", "You're beat! and yet, you must allow,",
+		  "I bore my house upon my back!");
+}
