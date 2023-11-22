dc5b53e72881a94d5b1030df404c654e4591c2c4
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-22 03:29:57 +0200

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,15 @@
 #include "lists.h"
+/**
+ * print_listint_safe - Prints a linked list safely to avoid infinite loop.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Description: This function prints the linked list nodes safely to avoid
+ * an infinite loop in case of a cycle in the list.
+ *
+ * Return: The number of nodes in the linked list.
+ * Author: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
 
 size_t print_listint_safe(const listint_t *head)
 {
