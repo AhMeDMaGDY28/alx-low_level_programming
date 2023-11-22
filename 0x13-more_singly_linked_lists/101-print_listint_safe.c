4a3efdf589fc121e4f63b9b0da2c54a1813363e7
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-11-22 03:29:24 +0200

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,1 +1,20 @@
 #include "lists.h"
+
+size_t print_listint_safe(const listint_t *head)
+{
+	if (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head->next < head)
+		{
+			return (1 + print_listint_safe(head->next));
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			return (1);
+		}
+	}
+	return (0);
+	exit(98);
+}
