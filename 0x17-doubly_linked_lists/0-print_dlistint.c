12ea99d06bb14b4e87ba6e84d675ac5b0188c40a
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-01-11 16:09:17 +0200

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,16 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - Print all the elements of a doubly linked list.
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * This function prints the integer elements of a doubly linked list.
+ * Each element is printed on a new line.
+ *
+ * Return: The number of nodes in the doubly linked list.
+ *
+ * Authors: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node_count = 0;
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * dlistint_len - Count the number of elements in a doubly linked list.
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * This function counts and returns the number of elements in a doubly linked list.
+ *
+ * Return: The number of elements in the doubly linked list.
+ *
+ * Authors: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t number_count = 0;
+
+	if (!h)
+		return (0);
+	while (h)
+	{
+		number_count++;
+		h = h->next;
+	}
+	return (number_count);
+}
