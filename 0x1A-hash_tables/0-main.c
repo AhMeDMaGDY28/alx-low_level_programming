19cab7b0df80d2dd11745348caa0916c9478fb84
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-03 00:33:04 +0200

    T1

--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-
+#include <stdio.h>
 /**
  * main - check the code for
  *
--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,14 @@
+/**
+ * hash_djb2 - Computes the hash value for a given string using the djb2 algorithm.
+ * @str: The string to compute the hash value for.
+ *
+ * This function computes the hash value for a given string using the djb2 algorithm.
+ *
+ * Return: The computed hash value.
+ *
+ * Authors: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
