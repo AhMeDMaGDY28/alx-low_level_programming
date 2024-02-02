221b98f973d89bbd48e275640fceb5513179047e
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-03 00:35:45 +0200

    T1 fix

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,10 @@
 /**
- * hash_djb2 - Computes the hash value for a given string using the djb2 algorithm.
+ * hash_djb2 - Computes the hash value for
+ * a given string using the djb2 algorithm.
  * @str: The string to compute the hash value for.
  *
- * This function computes the hash value for a given string using the djb2 algorithm.
+ * This function computes the hash value for a
+ * given string using the djb2 algorithm.
  *
  * Return: The computed hash value.
  *
@@ -11,13 +13,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
