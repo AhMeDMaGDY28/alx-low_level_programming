8e57f26f407069a80372a31f3ee07cb2c489080c
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-02 22:36:38 +0200

    T0 fix betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,16 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the hash table.
+ *
+ * This function creates a hash table with a given size. If memory allocation
+ * fails, it returns NULL.
+ *
+ * Return: A pointer to the newly created hash table, or NULL if it fails.
+ *
+ * Authors: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *array_creator;
