98ca2a931584afa3f504c72c7e3168f95cf21fa0
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-03 03:56:57 +0200

    T5 fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,9 +1,22 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_print - Prints the elements of a hash table.
+ * @ht: A pointer to the hash table.
+ *
+ * This function prints the elements of a hash table in the following format:
+ * {key1: value1, key2: value2, ...}.
+ * It traverses each bucket of the hash table
+ * and prints the key-value pairs of each node in the linked list.
+ *
+ * Return: No return value.
+ *
+ * Authors: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
-	long unsigned int i;
+	unsigned long int i;
 	int flag = 0;
 
 	if (!ht)
