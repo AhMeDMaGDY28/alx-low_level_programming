9e2b6ecf7569380ebd25ff93e81a09f12ee48007
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-08 23:54:32 +0300

    2024-05-08 11:54:32 PM

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,10 +3,12 @@
 int binary_search(int *array, size_t size, int value)
 {
 	size_t i = 0, mid = ((size - 1) / 2), half, end, start;
+	int *p;
 
 	while (i < size)
 	{
 		half = mid;
+		*p = array[half];
 
 		printf("\n");
 		if (value == array[half])
@@ -14,21 +16,20 @@ int binary_search(int *array, size_t size, int value)
 			return (half);
 		}
 		if (value == array[half + 1])
-		{
 			return (half + 1);
-		}
 		if (value == array[half - 1])
 		{
-			return (half - 1);
+			return (half -1);
 		}
-
+		
 		if (value < array[half])
 		{
 			end = half;
 			start = 0;
+
 			mid = (half / 2);
 			printf("Searching in array: ");
-			for (i = start; i < end; i++)
+			for (i = 0; i < end; i++)
 			{
 				printf("%d", array[i]);
 			}
